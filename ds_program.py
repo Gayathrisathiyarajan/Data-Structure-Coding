ff12584bbb6e552f96dfe551641aa7e7c853e8a1
Gayathri S <gayathrisathiyarajan@gmail.com>
2025-06-10 00:04:43 +0530

    Recursion and Backtracking

--- ds_program.py
+++ ds_program.py
@@ -1025,3 +1025,142 @@ def findSubsetsThatSumToK(arr, n, k):
 
 
 # Recursion and Backtracking
+
+# Problem Statement 1
+
+# You have been given a 9 X 9 2D matrix 'MATRIX' with some cells filled with digits(1 - 9), and some empty cells (denoted by 0).
+
+# You need to find whether there exists a way to fill all the empty cells with some digit(1 - 9) such that the final matrix is a valid Sudoku solution.
+
+# A Sudoku solution must satisfy all the following conditions-
+
+# 1. Each of the digits 1 - 9 must occur exactly once in each row.
+# 2. Each of the digits 1 - 9 must occur exactly once in each column.
+# 3. Each of the digits 1 - 9 must occur exactly once in each of the 9, 3 x 3 sub-matrices of the matrix.
+# Note
+# 1. There will always be a cell in the matrix which is empty.
+# 2. The given initial matrix will always be consistent according to the rules mentioned in the problem statement.
+# Detailed explanation ( Input/output format, Notes, Images )
+# Constraints:
+# 1 <= 'T' <= 5
+# N = 9
+# 0 <= MATRIX[i][j] <= 9
+
+# Where 'N' denotes the size of the given square matrix.
+
+# Time Limit: 1sec
+# Sample Input 1:
+# 1
+# 9 0 0 0 2 0 7 5 0 
+# 6 0 0 0 5 0 0 4 0 
+# 0 2 0 4 0 0 0 1 0 
+# 2 0 8 0 0 0 0 0 0 
+# 0 7 0 5 0 9 0 6 0 
+# 0 0 0 0 0 0 4 0 1 
+# 0 1 0 0 0 5 0 8 0 
+# 0 9 0 0 7 0 0 0 4 
+# 0 8 2 0 4 0 0 0 6
+# Sample Output 1:
+# yes
+# Explanation of the Sample Input1:
+# One of the possible solutions is:
+# 9 4 1 3 2 6 7 5 8
+# 6 3 7 1 5 8 2 4 9
+# 8 2 5 4 9 7 6 1 3
+# 2 6 8 7 1 4 3 9 5
+# 1 7 4 5 3 9 8 6 2
+# 3 5 9 6 8 2 4 7 1
+# 4 1 3 2 6 5 9 8 7
+# 5 9 6 8 7 3 1 2 4
+# 7 8 2 9 4 1 5 3 6
+# Sample Input 2:
+# 1
+# 1 5 9 0 0 6 0 3 2
+# 2 7 4 0 0 0 0 0 0
+# 3 8 6 2 0 0 0 0 5
+# 4 9 2 5 0 1 0 8 0
+# 6 3 7 0 4 0 0 0 0
+# 5 1 0 8 2 0 0 0 0
+# 8 2 1 0 0 0 0 0 0
+# 7 6 0 1 0 0 4 2 0
+# 9 4 3 0 7 0 0 6 1
+# Sample Output 2:
+# no
+# Explanation of the Sample Input2:
+# In the third column from the left, there are two empty cells out of which one has to be filled with ‘8’, but we can’t put 8 in any of those two cells.
+
+def isItSudoku(matrix):
+    # Write your code here.
+    def isValid(matrix, row, col, num):
+        # Check if num is in the current row
+        for x in range(9):
+            if matrix[row][x] == num:
+                return False
+        # Check if num is in the current column
+        for x in range(9):
+            if matrix[x][col] == num:
+                return False
+        # Check if num is in the current 3x3 box
+        startRow = row - row % 3
+        startCol = col - col % 3
+        for i in range(3):
+            for j in range(3):
+                if matrix[startRow + i][startCol + j] == num:
+                    return False
+        return True
+
+    def solve(matrix):
+        for row in range(9):
+            for col in range(9):
+                if matrix[row][col] == 0:
+                    for num in range(1, 10):
+                        if isValid(matrix, row, col, num):
+                            matrix[row][col] = num
+                            if solve(matrix):
+                                return True
+                            matrix[row][col] = 0
+                    return False
+        return True
+    return solve(matrix)
+
+
+# Problem Statement 2
+
+# Given a singly linked list of integers. Your task is to return the head of the reversed linked list.
+
+# For example:
+# The given linked list is 1 -> 2 -> 3 -> 4-> NULL. Then the reverse linked list is 4 -> 3 -> 2 -> 1 -> NULL and the head of the reversed linked list will be 4.
+# Follow Up :
+# Can you solve this problem in O(N) time and O(1) space complexity?
+# Detailed explanation ( Input/output format, Notes, Images )
+# Constraints :
+# 1 <= T <= 5
+# 0 <= L <= 10^5
+# 1 <= data <= 10^9 and data != -1
+
+# Time Limit: 1 sec
+# Sample Input 1 :
+# 1
+# 1 2 3 4 5 6 -1
+# Sample Output 1 :
+# 6 5 4 3 2 1 -1
+# Explanation For Sample Input 1 :
+# For the first test case,  After changing the next pointer of each node to the previous node, The given linked list is reversed.
+# Sample Input 2 :
+# 1
+# 10 21 3 2 4 -1
+# Sample Output 2 :
+# 4 2 3 21 10 -1
+
+
+def reverseLinkedList(head):
+    # Write your code here.
+    prev = None
+    curr = head
+    while curr:
+        next_node = curr.next  
+        curr.next = prev       
+        prev = curr
+        curr = next_node
+    return prev
+
