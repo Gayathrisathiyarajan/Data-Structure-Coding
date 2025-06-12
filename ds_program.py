444bbd6d6d9609731b1ab53ac63dbf2712447ed3
Gayathri S <gayathrisathiyarajan@gmail.com>
2025-06-13 00:09:52 +0530

    Reverse Stack Using Recursion.

--- ds_program.py
+++ ds_program.py
@@ -1595,3 +1595,40 @@ def generateString(N: int) -> List[str]:
             backtrack(current + '1', '1')
     backtrack("", '0')  # Start with empty string and last_char='0'
     return result
+
+
+# Problem Statement 10 -  Reverse Stack Using Recursion.
+# Problem statement
+# Reverse a given stack of 'N' integers using recursion. You are required to make changes in the input parameter itself.
+
+# Note: You are not allowed to use any extra space other than the internal stack space used due to recursion.
+
+# Example:
+# Input: [1,2,3,4,5] 
+# Output: [5,4,3,2,1]
+
+# Detailed explanation ( Input/output format, Notes, Images )
+# Sample Input 1 :
+# 3
+# 2 1 3
+# Sample Output 1 :
+# 3 1 2
+# Explanation to Sample Input 1 :
+# Reverse of a give stack is 3 1 2 where first element becomes last and last becomes first.                   
+# Sample Input 2 :
+# 2
+# 3 2
+# Sample Output 2 :
+# 2 3
+# Constraints :
+# 0 <= N <= 10^3
+# Where 'N' is the number of elements in the given stack.
+
+# Time Limit: 1 sec
+
+def reverseStack(stack: List[int]) -> None:
+    # Write your code here.
+    if stack:
+        x = stack.pop()
+        reverseStack(stack)
+        stack.insert(0, x)
