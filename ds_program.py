77afa3b48ca7231f1a070df6f7cde15075d5229b
Gayathri S <gayathrisathiyarajan@gmail.com>
2025-07-09 23:51:46 +0530

    Swap Two Numbers

--- ds_program.py
+++ ds_program.py
@@ -2094,3 +2094,43 @@ def reverseArray(arr, m):
             break
     return array_of_value
 
+
+# Problem statement - Swap Two Numbers
+# You are given two numbers 'a' and 'b' as input.
+
+# You must swap the values of 'a' and 'b'.
+
+# For Example:
+# Input: 
+# 'a' = 8, 'b' = 5
+
+# Output:
+# 5 8
+
+# Explanation:
+# Initially, the value of 'a' and 'b' is 8 and 5, respectively.
+
+# After swapping, the value of 'a' is 5, and the value of 'b' is 8.
+# Detailed explanation ( Input/output format, Notes, Images )
+# Sample Input 1:
+# 1 2 
+# Sample Output 1:
+# 2 1
+# Explanation For Sample Input 1:
+# The output of the above test case is 2 1.
+# Sample Input 2:
+# 5 6
+# Sample Output 2:
+# 6 5
+# Explanation For Sample Input 2:
+# The output of the above test case is  6 5.
+# Constraints:
+# -10^5 <= 'a', 'b' <= 10 ^ 5
+
+# Time Limit: 1 sec
+
+def swapNumber(a:list,  b: list) -> None:
+    # write your code here
+    c = a[0]
+    a[0] = b[0]
+    b[0] = c
