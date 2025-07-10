b4c7622671725e9124269fe08cedda751ffd9a50
Gayathri S <gayathrisathiyarajan@gmail.com>
2025-07-10 23:38:47 +0530

    First and Last Position of an Element In Sorted Array

--- ds_program.py
+++ ds_program.py
@@ -2134,3 +2134,63 @@ def swapNumber(a:list,  b: list) -> None:
     c = a[0]
     a[0] = b[0]
     b[0] = c
+
+
+# Problem Statement -  First and Last Position of an Element In Sorted Array
+# Problem statement
+# You have been given a sorted array/list 'arr' consisting of ‘n’ elements. You are also given an integer ‘k’.
+
+# Now, your task is to find the first and last occurrence of ‘k’ in 'arr'.
+# Note :
+# 1. If ‘k’ is not present in the array, then the first and the last occurrence will be -1. 
+# 2. 'arr' may contain duplicate elements.
+
+# Example:
+# Input: 'arr' = [0,1,1,5] , 'k' = 1
+
+# Output: 1 2
+
+# Explanation:
+# If 'arr' = [0, 1, 1, 5] and 'k' = 1, then the first and last occurrence of 1 will be 1(0 - indexed) and 2.
+# Detailed explanation ( Input/output format, Notes, Images )
+# Sample Input 1:
+# 8 2
+# 0 0 1 1 2 2 2 2
+
+# Sample output 1:
+# 4 7
+
+# Explanation of Sample output 1:
+# For this testcase the first occurrence of 2 in at index 4 and last occurrence is at index 7.
+
+# Sample Input 2:
+# 4 2
+# 1 3 3 5
+
+# Sample output 2:
+# -1 -1
+
+# Expected Time Complexity:
+# Try to do this in O(log(n)).
+
+# Constraints:
+# 1 <= n <= 10^5
+# 0 <= k <= 10^9
+# 0 <= arr[i] <= 10^9
+
+# Time Limit : 1 second
+
+
+def firstAndLastPosition(arr, n, k):
+    # Write your code here
+    first = -1
+    last = -1
+    for i in range(len(arr)):
+        if arr[i] == k:
+            if first == -1:
+                first = i
+            last = i
+    if first == -1:
+        return("-1 -1")
+    else:
+        return(first, last)
