beaa0e6822687ee65f6d6db48a98eb55399c5477
Gayathri S <gayathrisathiyarajan@gmail.com>
2025-08-04 22:09:07 +0530

    DSA

--- ds_program.py
+++ ds_program.py
@@ -976,34 +976,6 @@ def printSeries(n, k):
 
 # The order of elements in a particular subset should be in increasing order of the index.
 # Detailed explanation ( Input/output format, Notes, Images )
-# Constraints:
-# 1 <= 'N' <= 16
-# - (10 ^ 6) <= ARR[i] <= (10 ^ 6)
-# - 16 * (10 ^ 6) <= 'K' <= 16 * (10 ^ 6)
-
-# Where ‘ARR[i]’ denotes the value for ‘ith’ element of the array ‘ARR’ and 'K' is the given sum.
-
-# Time Limit: 1 sec.
-# Sample Input 1:
-# 3
-# 2 4 6
-# 6
-# Sample Output 1:
-# 2 4
-# 6
-# Explanation of the Sample Input 1:
-# For the array'ARR' = {2, 4, 6}, we can have subsets {}, {2}, {4}, {6}, {2, 4}, {2, 6}, {4, 6}, {2, 4, 6}. Out of these 8 subsets, {2, 4} and {6} sum to the given 'K' i.e. 6. 
-# Sample Input 2:
-# 6 
-# 5 -1 8 2 7 0
-# 7
-# Sample Output 2:
-# -1 8 
-# -1 8 0 
-# 5 2 
-# 5 2 0 
-# 7 
-# 7 0 
 
 def findSubsetsHelper(arr, index, current_subset, current_sum, target, result):
     # Base case
@@ -1041,53 +1013,6 @@ def findSubsetsThatSumToK(arr, n, k):
 # 1. There will always be a cell in the matrix which is empty.
 # 2. The given initial matrix will always be consistent according to the rules mentioned in the problem statement.
 # Detailed explanation ( Input/output format, Notes, Images )
-# Constraints:
-# 1 <= 'T' <= 5
-# N = 9
-# 0 <= MATRIX[i][j] <= 9
-
-# Where 'N' denotes the size of the given square matrix.
-
-# Time Limit: 1sec
-# Sample Input 1:
-# 1
-# 9 0 0 0 2 0 7 5 0 
-# 6 0 0 0 5 0 0 4 0 
-# 0 2 0 4 0 0 0 1 0 
-# 2 0 8 0 0 0 0 0 0 
-# 0 7 0 5 0 9 0 6 0 
-# 0 0 0 0 0 0 4 0 1 
-# 0 1 0 0 0 5 0 8 0 
-# 0 9 0 0 7 0 0 0 4 
-# 0 8 2 0 4 0 0 0 6
-# Sample Output 1:
-# yes
-# Explanation of the Sample Input1:
-# One of the possible solutions is:
-# 9 4 1 3 2 6 7 5 8
-# 6 3 7 1 5 8 2 4 9
-# 8 2 5 4 9 7 6 1 3
-# 2 6 8 7 1 4 3 9 5
-# 1 7 4 5 3 9 8 6 2
-# 3 5 9 6 8 2 4 7 1
-# 4 1 3 2 6 5 9 8 7
-# 5 9 6 8 7 3 1 2 4
-# 7 8 2 9 4 1 5 3 6
-# Sample Input 2:
-# 1
-# 1 5 9 0 0 6 0 3 2
-# 2 7 4 0 0 0 0 0 0
-# 3 8 6 2 0 0 0 0 5
-# 4 9 2 5 0 1 0 8 0
-# 6 3 7 0 4 0 0 0 0
-# 5 1 0 8 2 0 0 0 0
-# 8 2 1 0 0 0 0 0 0
-# 7 6 0 1 0 0 4 2 0
-# 9 4 3 0 7 0 0 6 1
-# Sample Output 2:
-# no
-# Explanation of the Sample Input2:
-# In the third column from the left, there are two empty cells out of which one has to be filled with ‘8’, but we can’t put 8 in any of those two cells.
 
 def isItSudoku(matrix):
     # Write your code here.
