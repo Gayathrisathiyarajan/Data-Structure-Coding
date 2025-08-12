8844dbaf452e0b9bd426314dbfc828b466b7d564
Gayathri S <gayathrisathiyarajan@gmail.com>
2025-08-12 22:07:02 +0530

    DSA

--- ds_program.py
+++ ds_program.py
@@ -30,38 +30,8 @@ print(arr)
 
 # Problem statement
 # You are given an array ‘arr’ containing ‘n’ integers. You are also given an integer ‘num’, and your task is to find whether ‘num’ is present in the array or not.
-
 # If ‘num’ is present in the array, return the 0-based index of the first occurrence of ‘num’. Else, return -1.
 
-# Example:
-# Input: ‘n’ = 5, ‘num’ = 4 
-# 'arr' =  [6,7,8,4,1] 
-
-# Output: 3
-
-# Explanation:
-# 4 is present at the 3rd index.
-# Detailed explanation ( Input/output format, Notes, Images )
-# Sample Input 1:
-# 5 4
-# 6 7 8 4 1
-# Sample Output 1 :
-# 3
-# Explanation Of Sample Input 1:
-# 4 is present at the 3rd index.
-# Sample Input 2:
-# 4 2
-# 2 5 6 2
-# Sample Output 2 :
-# 0
-# Explanation Of Sample Input 1:
-# 2 is not present in the given array.
-# Expected time complexity:
-# The expected time complexity is O(n).
-# Constraints:
-# 1  <= 'n' <= 10^6
-# 1 <= 'arr'[i] <= 1000
-# Time Limit: 1 sec
 
 def linearSearch(n: int, num: int, arr: [int]) -> int:
     # Write your code here.
@@ -75,23 +45,7 @@ def linearSearch(n: int, num: int, arr: [int]) -> int:
 # 2nd program
 # Problem statement
 # Given an array of size N, find the sum of its elements.
-
 # Detailed explanation ( Input/output format, Notes, Images )
-# Constraints:
-# 1 <= n <= 100000
-# -10000 <= ai <= 10000
-# Output Format
-# In single line, print one integer : sum of all the elements in the array
-# Sample Input 1:
-# 5
-# 1 2 3 4 5 
-# Sample Output 1:
-# 15
-# Sample Input 2:
-# 4
-# -1 5 3 0
-# Sample Output 2:
-# 7
 
 # your code goes here
 n = int(input())
